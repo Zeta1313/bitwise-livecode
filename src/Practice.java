5bf3e11fc332fb1a8386c8048c7c235fd2ac1dda
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2025-11-18 14:53:18 -0800

    11/18/25 class

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,27 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Yay bitwise operations!");
+        int color = 0xc42460;
+        int mask = 0x0000ff;
+
+        int blue = color & mask;
+
+        System.out.println(Integer.toHexString(blue));
+
+        int mask2 = 0x00ff00;
+        int green = (color & mask2) >> 0;
+
+        System.out.println(Integer.toHexString(green));
+        
+        //int x = 0b1010;
+        //int p = 0b1010;
+
+        //int q = x & p;
+
+        //System.out.println (Integer.toBinaryString(q));
+        //int y = 0xcf4e;
+        //int z = 94;
+
+        //System.out.println(Integer.toHexString(z));
+        //System.out.println(Integer.toBinaryString(z));
     }
 }
